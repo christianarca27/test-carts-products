863585d756376569fb0ab910a27b47322a0c35df
christianarca27 <christian.arca.mail@gmail.com>
2023-06-24 13:45:59 +0200

    feat: seeder Product

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Product;
+use Faker\Generator;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,8 +14,16 @@ class ProductSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Generator $faker)
     {
-        //
+        for ($i = 0; $i < 10; $i++) {
+            $newProduct = new Product();
+
+            $newProduct->SKU = $faker->regexify('[A-Z0-9]{12}');
+            $newProduct->name = $faker->word();
+            $newProduct->price = $faker->randomFloat(2, 0, 100);
+
+            $newProduct->save();
+        }
     }
 }
