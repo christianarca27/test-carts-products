33dc9cda7b43e9ff7b380ca4e47f9debc5793296
christianarca27 <christian.arca.mail@gmail.com>
2023-06-25 17:53:57 +0200

    feat: migliorate prestazioni ProductSeeder

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -6,6 +6,7 @@ use App\Models\Product;
 use Faker\Generator;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use FakerRestaurant\Provider\it_IT\Restaurant as FakerRestaurant;
 
 class ProductSeeder extends Seeder
 {
@@ -16,12 +17,17 @@ class ProductSeeder extends Seeder
      */
     public function run(Generator $faker)
     {
+        $faker->addProvider(new FakerRestaurant($faker));
         for ($i = 0; $i < 10; $i++) {
             $newProduct = new Product();
 
             $newProduct->SKU = $faker->regexify('[A-Z0-9]{12}');
-            $newProduct->name = $faker->word();
-            $newProduct->price = $faker->randomFloat(2, 0, 100);
+
+            do {
+                $newProduct->name = $faker->foodName();
+            } while (Product::where('name', $newProduct->name)->first());
+
+            $newProduct->price = $faker->randomFloat(2, 5, 20);
 
             $newProduct->save();
         }
